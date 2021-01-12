15f0e0ef4768820026dcafe44da1acfbe72b27fc
hmtmcse <hmtm.cse@gmail.com>
2021-01-12 09:25:26 +0600

    Configuration

--- src/main/java/com/problemfighter/pfspring/jwt/processor/JwtConfig.java
+++ src/main/java/com/problemfighter/pfspring/jwt/processor/JwtConfig.java
@@ -1,20 +1,47 @@
 package com.problemfighter.pfspring.jwt.processor;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties(prefix = "pf.jwt")
+@Configuration
+@ConfigurationProperties("pf.jwt")
 public class JwtConfig {
 
-    private String secretKey;
-    private String algorithm;
-    private Integer validityMinutes = 4321;
+    public String secretKey;
+    public String algorithm = "HMAC256";
+    public Integer validityMinutes = 4321;
 
     public JwtConfig() {}
 
     public String getAuthorizationHeader() {
         return HttpHeaders.AUTHORIZATION;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public Integer getValidityMinutes() {
+        return validityMinutes;
+    }
+
+    public void setValidityMinutes(Integer validityMinutes) {
+        this.validityMinutes = validityMinutes;
     }
 }
