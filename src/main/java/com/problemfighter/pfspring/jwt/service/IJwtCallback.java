edaf216ded5bf3849ca666c88f1343d6c6bc4c36
hmtmcse <hmtm.cse@gmail.com>
2021-01-18 08:07:14 +0600

    identity implementation

--- src/main/java/com/problemfighter/pfspring/jwt/service/IJwtCallback.java
+++ src/main/java/com/problemfighter/pfspring/jwt/service/IJwtCallback.java
@@ -1,7 +1,6 @@
 package com.problemfighter.pfspring.jwt.service;
 
 
-import com.problemfighter.pfspring.jwt.processor.JwtProcessor;
 
 public interface IJwtCallback {
     public void onCreate(JwtService jwtService);
